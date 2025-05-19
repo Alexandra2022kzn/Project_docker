7e34b138dd5455c0815a3110d7bc5cdeca2ac62f
Alexandra2022kzn <alexandra@DESKTOP-NG99BTO>
2025-05-19 19:11:12 +0300

    Добавлены лог-файлы и файлы с результатом выполнения кода. Также дописан README

--- FP_SNPs/FP_SNP.py
+++ FP_SNPs/FP_SNP.py
@@ -7,15 +7,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO, filename="FP_SNP_log.log",filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
-logging.info("An INFO")
 
 parser = argparse.ArgumentParser(description='Преобразование VCF файла с определением референсного и альтернативного аллеля')
 parser.add_argument('--in_file', type=str, required=True, help='Имя входного файла')
 parser.add_argument('--out_file', type=str, required=True, help='Имя выходного файла')
+parser.add_argument('--chr_dir', type=str, required=True, help='Папка с референсами хромосом')
 args = parser.parse_args()
 
 def get_ref(chr, pos):
-    fa = FastaFile(f'sepChrs/{chr}.fa')
+    fa = FastaFile(f'{args.chr_dir}/{chr}.fa')
     return fa.fetch(chr, pos-1, pos)
 
 def get_alt(row):
@@ -31,25 +31,26 @@ try:
     with open(args.in_file, 'r') as file:
         first_line = file.readline().rstrip('\n')
         if not first_line:
+            logging.error("ValueError : Файл пустой",exc_info=True)
             raise ValueError("Файл пустой")
         if first_line != '#CHROM\tPOS\tID\tallele1\tallele2':
+            logging.error("ValueError : Неверный формат файла",exc_info=True)
             raise ValueError("Неверный формат файла")
         
         data = pd.read_csv(args.in_file, sep='\t')
-        data = data[:10]
         
         logging.info("Получение референсных аллелей...")
         data['REF'] = data.apply(lambda row: get_ref(row["#CHROM"], row["POS"]), axis=1)
-        data['REF'].unique() # можно использовать для проверки, что нет никаких лишних букв/символов
-
+        logging.info(f"Референсные аллели получены. Уникальные значения: {data['REF'].unique()}")
+        
         logging.info("Определение альтернативных аллелей...")
         data["ALT"] = data.apply(get_alt, axis=1)
 
         data.drop(columns=['allele1' ,'allele2'], inplace = True)
-        print(data['ALT'].value_counts())
+        logging.info(f"Распределение полученных аллелей: {data['ALT'].value_counts()}")
         data.to_csv(args.out_file, index=False, sep='\t')
         logging.info("Конец обработки.")
 
 except FileNotFoundError:
+    logging.error("FileNotFoundError : Файл отсутствует",exc_info=True)
     print('Файл отсутствует')
-
